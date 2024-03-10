24e16c58bc6168eef1047a9c57981dfdbd9ddaed
hsBUPT <2257757362@qq.com>
2024-03-10 12:51:02 +0800

    Updeting...

--- app/src/main/java/com/example/air/IndexActivity.java
+++ app/src/main/java/com/example/air/IndexActivity.java
@@ -2,8 +2,10 @@ package com.example.air;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class IndexActivity extends AppCompatActivity {
-    //private TextView imageView5,imageView6,imageView7,imageView8,imageView9,imageView10;
+    private ImageView imageView5,imageView6,imageView7,imageView8,imageView9,imageView10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,68 @@ public class IndexActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_index);
 
+        initView();
+        setVariable();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void initView(){
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+        imageView10 = findViewById(R.id.imageView10);
+    }
+
+    //各类参数的详情页面跳转
+    private void setVariable(){
+        //Co2
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IndexActivity.this,CO2Activity.class));
+            }
+        });
+        //Sound
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IndexActivity.this,SoundActivity.class));
+            }
+        });
+        //Temp
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IndexActivity.this,TempActivity.class));
+            }
+        });
+        //Humidity
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IndexActivity.this,TempActivity.class));
+            }
+        });
+        //Video
+        imageView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IndexActivity.this,VideoActivity.class));
+            }
+        });
+        //AutoControl
+        imageView10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IndexActivity.this,AutoActivity.class));
+            }
         });
     }
 
